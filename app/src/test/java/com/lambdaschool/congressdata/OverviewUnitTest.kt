4614e6f1636248243f23db966aa2ab2c58f545c7
Patrick Chow <PatrickChow0803@gmail.com>
2019-10-04 13:35:17 -0400

    Started basic Unit Testing for OfficialOverview

---
+++ app/src/test/java/com/lambdaschool/congressdata/OverviewUnitTest.kt
@@ -0,0 +1,21 @@
+package com.lambdaschool.congressdata
+
+//Goal of this test is to test the class "OfficialOverview"
+class OverviewUnitTest{
+
+    /*
+    OfficialOverview's constructor as reference
+    constructor(firstName: String, middleName: String, lastName: String, party: String, state: String, id: String) {
+        this.firstName = firstName
+        this.middleName = middleName
+        this.lastName = lastName
+        this.party = party
+        this.state = state
+        this.displayName = buildDisplayName()
+        this.id = id
+    }
+     */
+
+    
+
+}
