9275e5bafa7ed9b7037a1b67de2b7e6ad629efb0
Patrick Chow <PatrickChow0803@gmail.com>
2019-10-04 14:29:20 -0400

    Added UI Test and more testing dependencies

--- app/src/androidTest/java/com/lambdaschool/congressdata/DetailsActivityTest.kt
+++ app/src/androidTest/java/com/lambdaschool/congressdata/DetailsActivityTest.kt
@@ -1,22 +1,36 @@
 package com.lambdaschool.congressdata
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import org.junit.FixMethodOrder
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.MethodSorters
 
 @RunWith(AndroidJUnit4::class)
-@LargeTest //Allows Network Access
+@LargeTest // Allows Network Access
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) // Perform the testes in ascending order
 
 class DetailsActivityTest {
 
-    //Tells Android Studio which activity to use for testing
+    // Tells Android Studio which activity to use for testing
     @Rule
     @JvmField
     var activityScenarioRule = ActivityScenarioRule(DetailsActivity::class.java)
 
+    // Check to see if the text matches up with activity_details_view
+    @Test
+    fun check(){
+        onView(withId(R.id.profile_name)).check(matches(withText("Trent P. Kelly")))
+        onView(withId(R.id.profile_party)).check(matches(withText("Republican")))
+        onView(withId(R.id.profile_district)).check(matches(withText("Mississippi - District 10")))
+        onView(withId(R.id.profile_twitter)).check(matches(withText("Twitter")))
+        onView(withId(R.id.profile_facebook)).check(matches(withText("Facebook")))
+    }
 }
